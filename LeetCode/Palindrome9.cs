42262fce5e4927d1cdbdaa309fe4eb3e24dd9f10
SimonovIvan1 <ivan.happy@mail.ru>
2023-03-21 23:05:18 +0300

    Add project files.

---
+++ LeetCode/Palindrome9.cs
@@ -0,0 +1,19 @@
+﻿public class Palindrome9
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) { return false; }
+        if (x < 10) { return true; }
+
+        var temp = x;
+        var b = 0;
+        var digit = 0;
+        while (temp != 0)
+        {
+            digit = temp % 10;
+            b = b * 10 + digit;
+            temp /= 10;
+        }
+        return x == b;
+    }
+}
---
+++ LeetCode/TwoSum1.cs
@@ -0,0 +1,38 @@
+﻿public class TwoSum1
+{
+    //first method
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i <= nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j <= nums.Length - 1; j++)
+            {
+                if ((nums[i] + nums[j]) == target)
+                {
+                    return new int[2] { i, j };
+                }
+            }
+        }
+        return new int[2];
+    }
+    //second method with dictionary
+    public int[] TwoSumWithDictionary(int[] nums, int target)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (dic.ContainsKey(complement))
+            {
+                return new int[2] { i, dic[complement] };
+            }
+            if (!dic.ContainsKey(nums[i]))
+            {
+                //every distinct value only needs to exist once(one time)
+                dic.Add(nums[i], i);
+            }
+
+        }
+        return new int[] { };
+    }
+}
